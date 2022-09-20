b0a5fbbc11da65eec944660812602251ca16cf17
mumbimuthiga <veronicmuthiga@gmail.com>
2022-09-20 14:45:16 +0300

    Add files

---
+++ etc/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./styles/header.css" rel="stylesheet">
+    <link href="./styles/footer.css" rel="stylesheet">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+    <title>HRMS</title>
+</head>
+<body>
+   
+
+<div class="row">
+         <div class="col-sm-3">
+            <div class="logo">
+         
+            <img src="https://sajili.zetech.ac.ke/images/logo.png" alt="zetech university application online sajili">
+            </div>
+           
+            </div>
+        <div class="col-sm-6">
+        <h2>Zetech University Leave Management</h2>
+         </div>
+        <div class="col-sm-3">
+            <button class="btn btn-primary">Applicant Login</button>
+        </div> 
+</div> 
+
+    <div class="links" >
+        <h5><a href="https://sajili.zetech.ac.ke/index.php" class="active">Home</a></h5>
+        <h5><a href="https://sajili.zetech.ac.ke/index.php" class="">Apply Now</a></h5>
+        <h5><a href="https://sajili.zetech.ac.ke/index.php" class="">Contacts</a></h5>
+        <h5><a href="https://sajili.zetech.ac.ke/index.php" class="">About</a></h5>
+       
+    </div>
+   <div class="images">
+    <h1 style="margin-left:40%; !important">Zetech University</h1>
+   
+   </div>
+   <div class="container">
+  
+     <div class="main">
+    
+        <h4>Welcome to Leave Management - Zetech University Online Application</h4>
+        <button class="btn btn-primary">Apply Here</button>
+       
+    </div>
+    
+    
+  
+   
+       
+
+    
+    </div>
+
+<div class="footer-banner">
+<div class="col-sm-1"></div>
+    <div class="col-sm-4">
+        <img src="https://www.iconpacks.net/icons/1/free-phone-icon-1-thumb.png" style="width:50px;height:50px" alt="Phone Icon">
+        <h5>Call:0719034500</h5>
+        <h5>WhatsApp: 0706622557</h5>
+   
+    </div>
+
+    
+    <div class="col-sm-4">
+        <img src="https://cdn-icons-png.flaticon.com/512/1251/1251632.png" style="width:50px;height:50px" alt="Email Icon">
+        <h5>Email:online.application@zetech.ac.ke</h5>
+        <h5>Main Website: www.zetech.ac.ke</h5>
+     </div>
+
+     <div class="col-sm-3">
+        <img src="https://www.iconpacks.net/icons/2/free-location-icon-2950-thumb.png"  style="width:50px;height:50px" alt="loc Icon">
+        <h5>Zetech University,</h5>
+        <h5>Off Thika Road, Ruiru</h5>
+       
+
+     </div>
+
+</div>
+<div class="footer-link">
+    <div class="col-sm-6">
+        <img src="https://cdn2.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-whatsapp-circle-512.png" style="width:50px;height:50px" alt="whatsapp icon">
+        <a>Terms and Conditions</a>
+        <a>|</a>
+        <a>Main Website</a>
+    </div>
+    <div class="col-sm-6">
+        <p>Copyright © 2022. SAJILI - Zetech University Online Application</p>
+
+      
+    </div>
+    <div>
+    <img src=" https://cdn3.iconfinder.com/data/icons/n-webmaster-icon-set/512/10-512.png" style="width:50px;height:50px" alt="whatsapp icon">
+
+    </div>
+   
+
+</div>
+   
+    
+ 
+</body>
+</html>
